805769191c59a4539f57eee8b9e086bd5e1cd462
Hans-J. Schmid <knock@myopendoor.de>
2026-06-09 01:02:56 +0200

    3.5 Implementing Our First Integration Test

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,18 @@
+use actix_web::dev::Server;
 use actix_web::{App, HttpResponse, HttpServer, Responder, web};
 
 async fn health_check() -> impl Responder {
   HttpResponse::Ok()
 }
 
-pub async fn run() -> std::io::Result<()> {
-  HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+// Notice the different signature!
+// We return `Server` on the happy path and we dropped the `async` keyword
+// We have no .await call, so it is not needed anymore.
+
+pub fn run() -> Result<Server, std::io::Error> {
+  let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
     .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    .run();
+
+  Ok(server)
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,32 @@
+// `tokio::test` is the testing equivalent of `tokio::main`.
+// It also spares you from having to specify the `#[test]` attribute.
+
+#[tokio::test]
+async fn health_check_works() {
+  spawn_app();
+
+  let client = reqwest::Client::new();
+
+  let response = client
+    .get("http://127.0.0.1:8000/health_check")
+    .send()
+    .await
+    .expect("Failed to execute request.");
+
+  // asserts
+  assert!(response.status().is_success());
+  assert_eq!(Some(0), response.content_length());
+}
+
+// No .await call, therefore no need for `spawn_app` to be async now.
+// We are also running tests, so it is not worth it to propagate errors:
+// if we fail to perform the required setup we can just panic and crash
+// all the things.
+
+fn spawn_app() {
+  let server = zero2prod::run().expect("Failed to bind address");
+  // Launch the server as a background task
+  // tokio::spawn returns a handle to the spawned future,
+  // but we have no use for it here, hence the non-binding let
+  let _ = tokio::spawn(server);
+}
