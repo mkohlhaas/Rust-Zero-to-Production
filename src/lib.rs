fd43e3aef18c7b17da24365cea49f03fdf0fac4f
Hans-J. Schmid <knock@myopendoor.de>
2026-06-09 01:27:45 +0200

    3.5.1.1 Clean Up

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,14 @@
 use actix_web::dev::Server;
 use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use std::net::TcpListener;
 
 async fn health_check() -> impl Responder {
   HttpResponse::Ok()
 }
 
-// Notice the different signature!
-// We return `Server` on the happy path and we dropped the `async` keyword
-// We have no .await call, so it is not needed anymore.
-
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
   let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-    .bind("127.0.0.1:8000")?
+    .listen(listener)?
     .run();
 
   Ok(server)
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,32 +1,31 @@
-// `tokio::test` is the testing equivalent of `tokio::main`.
-// It also spares you from having to specify the `#[test]` attribute.
+use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
-  spawn_app();
-
+  // arrange
+  let address = spawn_app();
   let client = reqwest::Client::new();
 
+  // act
   let response = client
-    .get("http://127.0.0.1:8000/health_check")
+    .get(&format!("{}/health_check", &address))
     .send()
     .await
     .expect("Failed to execute request.");
 
-  // asserts
+  // assert
   assert!(response.status().is_success());
   assert_eq!(Some(0), response.content_length());
 }
 
-// No .await call, therefore no need for `spawn_app` to be async now.
-// We are also running tests, so it is not worth it to propagate errors:
-// if we fail to perform the required setup we can just panic and crash
-// all the things.
+type Url = String;
 
-fn spawn_app() {
-  let server = zero2prod::run().expect("Failed to bind address");
-  // Launch the server as a background task
-  // tokio::spawn returns a handle to the spawned future,
-  // but we have no use for it here, hence the non-binding let
+fn spawn_app() -> Url {
+  let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+  let port = listener.local_addr().unwrap().port();
+  let server = zero2prod::run(listener).expect("Failed to bind address");
   let _ = tokio::spawn(server);
+
+  format!("http://127.0.0.1:{}", port)
 }
